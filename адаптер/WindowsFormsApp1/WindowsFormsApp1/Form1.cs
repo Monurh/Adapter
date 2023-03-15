6e331ed5e1d3a72adb9d552268b46fdf79dc9449
Monurh <74192667+Monurh@users.noreply.github.com>
2023-03-15 10:03:12 +0200

    Add files via upload

---
+++ адаптер/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Car car = new Car();
+        Camel camel = new Camel();
+        Horse horse = new Horse();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IVehicle vehicle = new CamelToCarAdapter(car);
+            vehicle.Move();
+
+            vehicle = new HorseToCamelAdapter(camel);
+            vehicle.Move();
+
+            vehicle = new CamelToHorseAdapter(horse);
+            vehicle.Move();
+
+           /* if(checkBox1.Checked) 
+            {
+                IVehicle vehicle = new CamelToCarAdapter(car);
+                vehicle.Move();
+            }
+            else if(checkBox2.Checked) 
+            {
+                vehicle = new HorseToCamelAdapter(camel);
+                vehicle.Move();
+            }*/
+        }
+    }
+}
---
+++ адаптер/WindowsFormsApp1/WindowsFormsApp1/Vehicle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    interface IVehicle
+    {
+        void Move();
+    }
+
+    class Car
+    {
+        public void Drive() => MessageBox.Show("Рух по дорозі з твердим покриттям");
+    }
+
+    class Camel
+    {
+        public void Walk() => MessageBox.Show("Ходьба по піску");
+    }
+
+    class Horse
+    {
+        public void Gallop() => MessageBox.Show("Галопом по траві");
+    }
+
+    class CarToCamelAdapter : IVehicle
+    {
+        private readonly Camel _camel;
+
+        public CarToCamelAdapter(Camel camel) => _camel = camel;
+
+        public void Move() => _camel.Walk();
+    }
+
+    class CamelToCarAdapter : IVehicle
+    {
+        private readonly Car _car;
+
+        public CamelToCarAdapter(Car car) => _car = car;
+
+        public void Move() => _car.Drive();
+    }
+
+    class CamelToHorseAdapter : IVehicle
+    {
+        private readonly Horse _horse;
+
+        public CamelToHorseAdapter(Horse horse) => _horse = horse;
+
+        public void Move() => _horse.Gallop();
+    }
+
+    class HorseToCamelAdapter : IVehicle
+    {
+        private readonly Camel _camel;
+
+        public HorseToCamelAdapter(Camel camel) => _camel = camel;
+
+        public void Move() => _camel.Walk();
+    }
+}
